f44b5c75c9005d63d498c5886b05e7d2dbdb103d
mauks <michael.maukstad@yahoo.com>
2019-05-16 22:50:27 -0500

    adding to log func

--- vpnmanager.py
+++ vpnmanager.py
@@ -2,9 +2,21 @@
 
 #vpnmanager.py program to check and restart vpn connection
 
-import datetime
+import datetime, os
 
 #local log device
 def log(event):
     data = event
-    print(data)
+    date = datetime.datetime.now()
+    if os.path.isfile('/var/log/vpnmanagerlog'):
+        logfile = open('/var/log/vpnmanagerlog', 'a')
+        logfile.write(date)
+        logfile.write(event)
+        logfile.close()
+    else:
+        logfile = open('/var/log/vpnmanagerlog', 'w')
+        logfile.write(str(date))
+        logfile.write(event)
+        logfile.close()
+    print(date)
+    print(str(date))
