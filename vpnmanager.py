9059b04577399c12c6a925cca2952b3491dd6b12
mauks <michael.maukstad@yahoo.com>
2019-06-02 23:40:51 -0500

    added test for tun, and func to call openvpn command and config

--- vpnmanager.py
+++ vpnmanager.py
@@ -23,27 +23,51 @@ def log(event):
         logfile.write(str(event) + '\n')
         logfile.close()
 
+#func for starting vpn, changes active dir to openvpn dir
+def opvpn():
+    log('opvpn called to start vpn')
+    os.chdir('/etc/openvpn')
+    try:
+        subprocess.call([
+                'openvpn',
+                '/etc/openvpn/client.ovpn'
+               ])
+    except:
+        log('failed to run openvpn subprocess')
+
 #vpnconnection check
 
-locNetStatus = subprocess.check_output(['ping', '-c5', '-w20', '192.168.0.1'],
-       stderr=subprocess.STDOUT,
-       shell=True)
+locNetStatus = subprocess.check_output(
+        ['ping', '-c5', '-w20', '192.168.0.1']
+       )
 locNetConfig = subprocess.check_output('ifconfig')
 
 #by ping 
 #TODO fix error thrown when network off, ping does not
 #return correctly
+#TODO fix error in subprocess.check_output call
 try:
-    subprocess.check_output(['ping', '-c5', '-w20', '192.168.0.1'],
-           stderr=subprocess.STDOUT,
-           shell=True)
+    subprocess.check_output(
+            ['ping', '-c5', '-w20', '192.168.0.1']
+           )
     log('net up, data from ping')
     log(locNetStatus)
     log(locNetConfig)
+    network = 'up'
 except:
     log('net down, data from ping')
     log(locNetStatus)
     log(locNetConfig)
+    network = 'down'
 
 #by ifconfig
+#TODO add ifconfig test for tun0
+tun = str('tun0')
+locNetConfig = str(locNetConfig)
+if tun in locNetConfig:
+    log('tun0 up')
+else:
+    log('tun0 down, calling opvpn func')
+    opvpn()
+
 
