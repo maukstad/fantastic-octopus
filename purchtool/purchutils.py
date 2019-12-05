ba2b3085535375057c51618ab3f36429676198d8
maukstad <michael.maukstad@yahoo.com>
2019-12-05 05:28:40 +0000

    starting purchutils.py file layout

---
+++ purchtool/purchutils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import csv
+from os import listdir
+
+#basic utils for PURCH
+
+#TODO load csv docs for processing
+local_files = listdir()
+load_file_count = len(local_files)
+
+
+
+#TODO find parts in both PURCH and FUZE data with expiring dates
+
+
+
+#TODO find parts in PURCH but missing from FUZE to show lost parts
+
+
+
+#TODO find parts overallocated in PURCH
