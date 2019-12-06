5cebee994a3bb5b3625f7a16315ab30c31f71353
maukstad <michael.maukstad@yahoo.com>
2019-12-06 05:02:01 +0000

    adding auto file search and user selection to app

--- purchtool/purchutils.py
+++ purchtool/purchutils.py
@@ -10,6 +10,19 @@ local_files = listdir()
 load_file_count = len(local_files)
 
 
+#TODO user picks files
+print('Please choose the PURCH equipment report csv from the following ' 
+        + str(load_file_count) + ' files')
+n = 0
+while n < load_file_count:
+    print("(" + str(n) + ") " + local_files[n])
+    n = n + 1
+user_selection = input('please select number:')
+if int(user_selection) >= 0 and int(user_selection) <= load_file_count -1:
+    print("Please confirm " + local_files(int(user_selection)) 
+            + " is the PURCH equipment report csv")
+    input("Yes/No")
+
 
 #TODO find parts in both PURCH and FUZE data with expiring dates
 
